d162d30b51b3a8b39b71a126eae0ca5538f75672
Infernoid Patrulea <hey@erickdavid.net>
2025-04-01 17:53:28 -0600

    Revert "???"
    This reverts commit 884c399d531262c31643bf05d401bc77e06e6c87.

--- templates/pages/home.data.js
+++
@@ -1,8 +0,0 @@
-export default {
-	layout: "global",
-	permalink: "index.html",
-	title: data => data.home.title,
-	description: data => data.home.description,
-	image: data => data.home.image,
-	sitemap: 10
-}
--- templates/server/error404.data.js
+++
@@ -1,7 +0,0 @@
-export default {
-	layout: "base",
-	permalink: "404.html",
-	title: data => data.error404.title,
-	description: data => data.error404.description,
-	image: data => data.error404.image
-}
